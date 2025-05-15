42affc6854abf0f845a11aadb2220f550a1272c9
abdul-hade <ah5041430@gmail.com>
2025-05-16 03:23:04 +0500

    this is good

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("🎉 Deployment via GitHub Actions successful!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
